ca5c925a299f4b7cddd4d3cc61353bf3bc9df503
Dominic88888 <100322080+Dominic88888@users.noreply.github.com>
2022-04-04 15:55:25 -0500

    Add files via upload
    Credit Card Number Checker
    
    This was a small project from Codecademy. It takes a nested array of credit card numbers and returns an array of "bad numbers" (numbers that failed the Luhn algorithm test). This new array can then be run through a function that determines which credit card company each of the bad numbers corresponds to, and outputs this list into another array.

---
+++ main.js
@@ -0,0 +1,136 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+//const mystery6 = [1, 4, 6, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+// Add your functions below:
+function validateCred(number){
+  //returns true or false on validity of credit card number
+  let sum = 0;
+  for(let i=0; i<number.length;i++){
+    if(i%2 == 0){
+      sum+=number[number.length - (i+1)];
+    }else{
+      sum+=doublyThing(number[number.length - (i+1)]);
+    }
+  }
+  function doublyThing(thing){
+    let x = (2*thing);
+    if(x > 9){
+      return x-9;
+    }else{
+      return x;
+    }
+  }
+  if(sum%10 == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function findInvalidCards(bundle){
+  //returns nested array of invalid numbers
+  let inv = [];
+  for(let i=0;i<bundle.length;i++){
+    if(validateCred(bundle[i])==false){
+      inv.push(bundle[i]);
+    }
+  }
+return inv;
+}
+
+function idInvalidCardCompanies(invNums){
+  //returns an array of companies with invalid credit card numbers
+  let companies = [];
+  for(let i=0; i<invNums.length; i++){
+    switch (invNums[i][0]){
+      case 3:
+        if(companies.some(slot => slot == 'Amex')){
+          break;
+        }else{
+          companies.push('Amex');
+        }
+        break;
+      case 4:
+        if(companies.some(slot => slot == 'Visa')){
+          break;
+        }else{
+          companies.push('Visa');
+        }
+        break;
+      case 5:
+        if(companies.some(slot => slot == 'Mastercard')){
+          break;
+        }else{
+          companies.push('Mastercard');
+        }
+        break;
+      case 6:
+        if(companies.some(slot =>slot == 'Discover')){
+          break;
+        }else{
+          companies.push('Discover');
+        }
+        break;
+      default:
+        if(companies.some(slot => slot == 'Company not found')){
+          break;
+        }else{
+          companies.push('Company not found');
+        }
+        break;
+    }
+  }
+  return companies;
+}
+
+const badCards = findInvalidCards(batch);
+
+console.log(idInvalidCardCompanies(badCards));
+//console.log(badCards);
+
+/*
+console.log(validateCred(valid1));
+console.log(validateCred(valid2));
+console.log(validateCred(valid3));
+console.log(validateCred(valid4));
+console.log(validateCred(valid5));
+
+console.log(validateCred(invalid1));
+console.log(validateCred(invalid2));
+console.log(validateCred(invalid3));
+console.log(validateCred(invalid4));
+console.log(validateCred(invalid5));
+
+console.log(validateCred(mystery1));
+//false
+console.log(validateCred(mystery2));
+//true
+console.log(validateCred(mystery3));
+//false
+console.log(validateCred(mystery4));
+//false
+console.log(validateCred(mystery5));
+//true
+*/
